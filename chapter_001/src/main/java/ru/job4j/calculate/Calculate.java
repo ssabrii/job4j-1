2073336ae671e329702b03fffa6e03c68fd9b68d
Maxim <mailtime2010@gmail.com>
2018-12-18 17:39:23 -0500

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,14 @@
  package ru.job4j.calculate;
- 
+ /**
+ *Calculate
+ *
+ *@author Maxim Vrnsky
+ */
   public class Calculate {
+   /**
+ *Main.
+ *@param args -args.
+ */
      public static void main(String[] args) {
          System.out.println("Hello world");
      }
