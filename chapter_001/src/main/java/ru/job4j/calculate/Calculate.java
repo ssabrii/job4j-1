9257be8566b6f51a09359c94695d00ce1a7a75f2
Maxim <mailtime2010@gmail.com>
2018-12-19 06:25:21 -0500

    1.4. JUnit[#99384]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -15,4 +15,12 @@
      public static void main(String[] args) {
          System.out.println("Hello world");
      }
+	 /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
  } 
