60c9042a02c3849c010b8ff552d2ddadda55fc1c
Maxim <mailtime2010@gmail.com>
2018-12-21 06:25:35 -0500

    4.2. Максимум из двух чисел[#99348]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,20 @@
+package ru.job4j.max;
+
+/**
+ * @author Max Vanny.
+ * @version 1.0
+ * @since 0.1
+ */
+public class Max {
+    /**
+     * Определяет маскимальное число из двух.
+     *
+     * @param first  Первое число.
+     * @param second Второе число.
+     * @return Максимальное число.
+     */
+    public int max(int first, int second) {
+        return first > second ? first : second;
+    }
+}
+
---
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.max;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class MaxTest {
+    @Test
+    public void whenFirstLessSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(1, 2);
+        assertThat(result, is(2));
+    }
+
+    @Test
+    public void whenFirstBetterSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(2, 1);
+        assertThat(result, is(2));
+    }
+}
+
+
