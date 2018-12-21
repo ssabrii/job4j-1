b0731b24728474dd5e5dfb5d38f812002a4a42ed
Maxim <mailtime2010@gmail.com>
2018-12-21 08:14:52 -0500

    4.4. Максимум из трех чисел[#99349]

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -16,7 +16,20 @@ public class Max {
     public int max(int first, int second) {
         return (first >= second) ? first : second;
     }
+    /**
+     * Определяет маскимальное число из трёх.
+     *
+     * @param first  Первое число.
+     * @param second Второе число.
+     * @param third Третье число.
+     * @return Максимальное число.
+     */
 
-
+    public int max(int first, int second, int third) {
+        int maximum = this.max(first, second);
+        maximum = this.max(maximum, third);
+        return maximum;
+    }
 }
+
 
--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -1,8 +1,9 @@
 package ru.job4j.max;
 
 import org.junit.Test;
-import static org.junit.Assert.assertThat;
+
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class MaxTest {
     @Test
@@ -18,12 +19,35 @@ public class MaxTest {
         int result = maxim.max(2, 1);
         assertThat(result, is(2));
     }
+
     @Test
     public void whenFirstSameSecond() {
         Max maxim = new Max();
         int result = maxim.max(2, 2);
         assertThat(result, is(2));
     }
+
+    @Test
+    public void whenThirstBetterSecondFirst() {
+        Max maxim = new Max();
+        int result = maxim.max(1, 2, 3);
+        assertThat(result, is(3));
+    }
+    @Test
+    public void whenFirstBetterSecondThird() {
+        Max maxim = new Max();
+        int result = maxim.max(3, 2, 1);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenSecondBetterFirstThird() {
+        Max maxim = new Max();
+        int result = maxim.max(2, 3, 1);
+        assertThat(result, is(3));
+    }
 }
+
+
 
 
