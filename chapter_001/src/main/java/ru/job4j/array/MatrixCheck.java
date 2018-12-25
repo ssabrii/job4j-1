e3aba71237e784a660e746e646fcf16275bf4873
Maxim <mailtime2010@gmail.com>
2018-12-25 10:37:12 -0500

    6.7. Квадратный массив заполнен true или false по диагоналям.[#99365][#100260]

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -21,7 +21,7 @@ public class MatrixCheck {
                 result = false;
                 break;
             }
-            if (data[0][0] != data[data.length - i - 1][i]) {
+            if (data[0][data.length - 1] != data[i][data.length - 1 - i]) {
                 result = false;
                 break;
             }
--- chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -1,8 +1,10 @@
 package ru.job4j.array;
 
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+
 /**
  * MatrixCheckTest.
  *
@@ -22,16 +24,28 @@ public class MatrixCheckTest {
         boolean result = check.mono(input);
         assertThat(result, is(true));
     }
+
     @Test
     public void whenDataNotMonoByTrueThenFalse() {
         MatrixCheck check = new MatrixCheck();
-        boolean[][] input = new boolean[][] {
+        boolean[][] input = new boolean[][]{
                 {true, true, false},
                 {false, false, true},
                 {true, false, true}
         };
         boolean result = check.mono(input);
         assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenDataMonoByTwoTrueThenTwoFalse() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][]{
+                {true, false},
+                {false, true}
+        };
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
     }
 }
 
