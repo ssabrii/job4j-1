dd167c603363958d42bea41f2e84460c3e45dc53
Maxim <mailtime2010@gmail.com>
2019-01-01 11:18:21 -0500

    5. Рефакторинг теста @Before @After[#99418]

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.pseudo;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -19,17 +21,18 @@ public class PaintTest {
     private final PrintStream stdout = System.out;
     private final ByteArrayOutputStream out = new ByteArrayOutputStream();
 
+    @Before
     public void loadOutput() {
         System.setOut(new PrintStream(this.out));
     }
 
+    @After
     public void backOutput() {
         System.setOut(this.stdout);
     }
 
     @Test
     public void whenDrawSquare() {
-        this.loadOutput();
         new Paint().draw(new Square());
         assertThat(new String(out.toByteArray()), is(new StringBuilder()
                         .append("+++++")
@@ -43,12 +46,10 @@ public class PaintTest {
                         .toString()
                 )
         );
-        this.backOutput();
     }
 
     @Test
     public void whenDrawTriangle() {
-        this.loadOutput();
         new Paint().draw(new Triangle());
         assertThat(new String(out.toByteArray()), is(
                 new StringBuilder()
@@ -63,6 +64,5 @@ public class PaintTest {
                         .toString()
                 )
         );
-        this.backOutput();
     }
 }
