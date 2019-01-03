cd932b5e6730370c20b6200bc3ea1fdc169811fc
Maxim <mailtime2010@gmail.com>
2019-01-03 17:07:17 -0500

    1. Создать действия UserAction[#99442] FindById

--- chapter_002/src/main/java/ru/job4j/tracker/FindItemById.java
+++ chapter_002/src/main/java/ru/job4j/tracker/FindItemById.java
@@ -3,39 +3,70 @@ package ru.job4j.tracker;
 import ru.job4j.models.Item;
 import ru.job4j.start.Input;
 
+/**
+ * FindItemById.
+ *
+ * @author Maxim Vanny.
+ * @version 2.0
+ * @since 0.1
+ */
 public class FindItemById implements UserAction {
+    /**
+     * The hold key.
+     */
     private int keyholder;
+    /**
+     * The string menu on the hold key.
+     */
     private String menu;
-    private boolean check = false;
-    private StringBuilder infotext = new StringBuilder();
-    private Item byId;
 
+    /**
+     * Constructor.
+     *
+     * @param keyholder the hold key.
+     * @param menu      the string menu on the hold key.
+     */
     public FindItemById(int keyholder, String menu) {
         this.keyholder = keyholder;
         this.menu = menu;
     }
+
+    /**
+     * Override method get the hold key.
+     *
+     * @return the hold key.
+     */
 
     @Override
     public int key() {
         return 4;
     }
 
+    /**
+     * Override method finds Item by ID.
+     *
+     * @param input   объект типа Input.
+     * @param tracker объект типа Tracker.
+     */
     @Override
     public void execute(Input input, Tracker tracker) {
         String id = input.ask("Поиск, введите ID заявки: ");
-        this.byId = tracker.findById(id);
-        this.check = byId != null;
+        Item byId = tracker.findById(id);
+        if (byId == null) {
+            System.out.println("Заявка не обнаружена. Уточните ID");
+        } else {
+            System.out.println(byId);
+        }
     }
 
+    /**
+     * Override method shows element menu.
+     *
+     * @return string element menu.
+     */
     @Override
     public String info() {
-        if (!this.check) {
-            infotext.append("Заявка не обнаружена. Уточните ID");
-            infotext.append(System.lineSeparator());
-        } else {
-            infotext.append(this.byId);
-            infotext.append(System.lineSeparator());
-        }
-        return this.infotext.toString();
+
+        return "4.Find by Id";
     }
 }
