3c6515fb2d43c34b4c8ff6915d5a8f4c34f2a8f1
Maxim <mailtime2010@gmail.com>
2019-01-03 17:05:42 -0500

    1. Создать действия UserAction[#99442] ShowItems

--- chapter_002/src/main/java/ru/job4j/tracker/ShowItems.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ShowItems.java
@@ -4,27 +4,62 @@ import ru.job4j.start.Input;
 
 import java.util.Arrays;
 
+/**
+ * ShowItem.
+ *
+ * @author Maxim Vanny.
+ * @version 2.0
+ * @since 0.1
+ */
 public class ShowItems implements UserAction {
+    /**
+     * The hold key.
+     */
     private int keyholder;
+    /**
+     * The string menu on the hold key.
+     */
     private String menu;
 
+    /**
+     * Constructor.
+     *
+     * @param keyholder the hold key.
+     * @param menu      the string menu on the hold key.
+     */
     public ShowItems(int keyholder, String menu) {
         this.keyholder = keyholder;
         this.menu = menu;
     }
 
+    /**
+     * Override method get the hold key.
+     *
+     * @return the hold key.
+     */
     @Override
     public int key() {
         return 1;
     }
 
+    /**
+     * Override method shows all Items.
+     *
+     * @param input   объект типа Input.
+     * @param tracker объект типа Tracker.
+     */
     @Override
     public void execute(Input input, Tracker tracker) {
         System.out.println(Arrays.toString(tracker.findAll()));
     }
 
+    /**
+     * Override method shows element menu.
+     *
+     * @return string element menu.
+     */
     @Override
     public String info() {
-        return "All Items from Storage.";
+        return "1.Show all item.";
     }
 }
