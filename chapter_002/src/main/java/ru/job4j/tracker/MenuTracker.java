f63c0d5a9ae89a96ba9a63d1e493e72478854a4c
Maxim <mailtime2010@gmail.com>
2019-01-03 17:10:46 -0500

    1. Создать действия UserAction[#99442] MenuTracker

---
+++ chapter_002/src/main/java/ru/job4j/tracker/MenuTracker.java
@@ -0,0 +1,81 @@
+package ru.job4j.tracker;
+
+import ru.job4j.start.Input;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * MenuTracker.
+ *
+ * @author Maxim Vanny.
+ * @version 2.0
+ * @since 0.1
+ */
+public class MenuTracker {
+    /**
+     * @param хранит ссылку на объект .
+     */
+    private Input input;
+    /**
+     * @param хранит ссылку на объект .
+     */
+    private Tracker tracker;
+    /**
+     * @param хранит ссылку на массив типа UserAction.
+     */
+    private List<UserAction> actions = new ArrayList<>();
+
+    /**
+     * Конструктор.
+     *
+     * @param input   объект типа Input
+     * @param tracker объект типа Tracker
+     */
+    public MenuTracker(Input input, Tracker tracker) {
+        this.input = input;
+        this.tracker = tracker;
+    }
+
+    /**
+     * Метод для получения размера списка меню.
+     *
+     * @return размер списка
+     */
+    public int getActionsSize() {
+        return this.actions.size();
+    }
+
+    /**
+     * Метод заполняет список.
+     */
+    public void fillActions() {
+        this.actions.add(new AddItem(0, "Add new Item"));
+        this.actions.add(new ShowItems(1, "Show all items"));
+        this.actions.add(new UpdateItem(2, "Edit item"));
+        this.actions.add(new DeleteItem(3, "Delete item"));
+        this.actions.add(new FindItemById(4, "Find item by Id"));
+        this.actions.add(new FindItemsByName(5, "Find items by name"));
+        this.actions.add(new ExitProgram(6, "Exit Program"));
+    }
+
+    /**
+     * Метод в зависимости от указанного ключа, выполняет соответствующие действие.
+     *
+     * @param key ключ операции
+     */
+    public void select(int key) {
+        this.actions.get(key).execute(this.input, this.tracker);
+    }
+
+    /**
+     * Метод выводит на экран меню.
+     */
+    public void show() {
+        for (UserAction action : this.actions) {
+            if (action != null) {
+                System.out.println(action.info());
+            }
+        }
+    }
+}
