192dfd6f2ab6163d5d5332287fde157af39f15e0
Maxim <mailtime2010@gmail.com>
2019-01-03 16:34:51 -0500

    1. Создать действия UserAction[#99442] AddItem

--- chapter_002/src/main/java/ru/job4j/tracker/AddItem.java
+++ chapter_002/src/main/java/ru/job4j/tracker/AddItem.java
@@ -3,41 +3,69 @@ package ru.job4j.tracker;
 import ru.job4j.models.Item;
 import ru.job4j.start.Input;
 
-public class AddItem implements UserAction {
-    private int keyholder;
-    private String menu;
-    private boolean check = false;
-    private StringBuilder infotext = new StringBuilder();
+/**
+ * AddItem.
+ *
+ * @author Maxim Vanny.
+ * @version 2.0
+ * @since 0.1
+ */
 
+public class AddItem implements UserAction {
+    /**
+     * The hold key.
+     */
+    private int keyholder;
+    /**
+     * The string menu on the hold key.
+     */
+    private String menu;
+
+    /**
+     * Constructor.
+     *
+     * @param keyholder the hold key.
+     * @param menu      the string menu on the hold key.
+     */
     public AddItem(int keyholder, String menu) {
         this.keyholder = keyholder;
         this.menu = menu;
     }
 
+    /**
+     * Override method get the hold key.
+     *
+     * @return the hold key.
+     */
     @Override
     public int key() {
         return 0;
     }
 
+    /**
+     * Override method adds new Item.
+     *
+     * @param input   объект типа Input.
+     * @param tracker объект типа Tracker.
+     */
     @Override
     public void execute(Input input, Tracker tracker) {
         System.out.println("--------- Добавление новой заявки -----------");
         String name = input.ask("Введите имя заявки: ");
         String desc = input.ask("Введите описание заявки: ");
         Item item = new Item(name, desc);
-        this.check = tracker.add(item) != null;
-        if (!this.check) {
-            infotext.append("Заявка не добавлена. Хранилище полное.");
-            infotext.append(System.lineSeparator());
+        if (tracker.add(item) != null) {
+            System.out.println("Заявка не добавлена. Хранилище полное.");
         } else {
-            infotext.append("----- Новая заявка с ID: ");
-            infotext.append(item.getId());
-            infotext.append("----");
-            infotext.append(System.lineSeparator());
+            System.out.println("----- Новая заявка с ID: " + item.getId() + "----");
         }
-        this.infotext.toString();
     }
 
+    /**
+     * Override method shows element menu.
+     *
+     * @return string element menu.
+     */
     @Override
     public String info() {
         return "0.Add new Item.";
