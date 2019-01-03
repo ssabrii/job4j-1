2f655adbc7e8f91da2efb3d234b943b487ea1234
Maxim <mailtime2010@gmail.com>
2019-01-03 17:06:45 -0500

    1. Создать действия UserAction[#99442] DeleteItem

--- chapter_002/src/main/java/ru/job4j/tracker/DeleteItem.java
+++ chapter_002/src/main/java/ru/job4j/tracker/DeleteItem.java
@@ -2,37 +2,67 @@ package ru.job4j.tracker;
 
 import ru.job4j.start.Input;
 
+/**
+ * DeleteItem.
+ *
+ * @author Maxim Vanny.
+ * @version 2.0
+ * @since 0.1
+ */
 public class DeleteItem implements UserAction {
+    /**
+     * The string menu on the hold key.
+     */
     private int keyholder;
+    /**
+     * The string menu on the hold key.
+     */
     private String menu;
-    private boolean check = false;
-    private StringBuilder infotext = new StringBuilder();
 
+    /**
+     * Constructor.
+     *
+     * @param keyholder the hold key.
+     * @param menu      the string menu on the hold key.
+     */
     public DeleteItem(int keyholder, String menu) {
         this.keyholder = keyholder;
         this.menu = menu;
     }
 
+    /**
+     * Override method get the hold key.
+     *
+     * @return the hold key.
+     */
     @Override
     public int key() {
         return 3;
     }
 
+    /**
+     * Override method delete Item.
+     *
+     * @param input   объект типа Input.
+     * @param tracker объект типа Tracker.
+     */
     @Override
     public void execute(Input input, Tracker tracker) {
         String id = input.ask("Введите ID удаляемой заявки: ");
-        this.check = tracker.delete(id);
+        if (!tracker.delete(id)) {
+            System.out.println("Заявка не удалена. Уточните ID заявки.");
+        } else {
+            System.out.println("Заявка " + id + " удалена.");
+        }
     }
 
+    /**
+     * Override method shows element menu.
+     *
+     * @return string element menu.
+     */
     @Override
     public String info() {
-        if (!this.check) {
-            this.infotext.append("Заявка не удалена. Уточните ID заявки.");
-            this.infotext.append(System.lineSeparator());
-        } else {
-            this.infotext.append("Заявка удалена.");
-            this.infotext.append(System.lineSeparator());
-        }
-        return this.infotext.toString();
+        return "3.Delete Item.";
     }
 }
