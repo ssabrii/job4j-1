9398ff33465fd435a5d623b15eec84bf0f752d9c
Maxim <mailtime2010@gmail.com>
2019-01-08 08:12:26 -0500

    1. Singleton.[#99454] порядок загрузки членов класса

---
+++ chapter_002/src/main/java/ru/job4j/example/LoadOrder.java
@@ -0,0 +1,85 @@
+package ru.job4j.example;
+
+/**
+ * LoadOrder.
+ *
+ * @author Maxim Vanny.
+ * @version 2.0
+ * @since 0.1
+ */
+public class LoadOrder {
+    /**
+     * конечное статическое поле.
+     */
+    private static final String STATIC_FINAL_FIELD = echo("static final field");
+    /**
+     * статическое поле.
+     */
+    private static String staticfield = echo("static field");
+
+    //private static final LoadOrder INSTANCE = new LoadOrder("static inner field");
+
+    //статический блок.
+    static {
+        echo("static block");
+    }
+
+    /**
+     * конечное поле класса.
+     */
+    private final String name = echo("final field");
+    /**
+     * поле класса.
+     */
+    private String surname = echo("field");
+
+    //не статической блок класса.
+    {
+        echo("not-static block");
+    }
+
+    /**
+     * Конструктор.
+     *
+     * @param msg текст.
+     */
+    public LoadOrder(final String msg) {
+        echo("constructor " + msg);
+    }
+
+    /**
+     * Метод выводить текст.
+     *
+     * @param text text.
+     * @return text.
+     */
+    public static String echo(final String text) {
+        System.out.println(text);
+        return text;
+    }
+
+    /**
+     * Start programm.
+     *
+     * @param args string.
+     */
+    public static void main(final String[] args) {
+        LoadOrder order = new LoadOrder("main");
+    }
+
+    /**
+     * inner static final class Holder.
+     *
+     * @author Maxim Vanny.
+     * @version 2.0
+     * @since 0.1
+     */
+
+
+    private static final class Holder {
+        /**
+         * Create oblect LoadOrder.
+         */
+        private static final LoadOrder INSTANCE = new LoadOrder("static inner field");
+    }
+}
