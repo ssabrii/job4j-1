1a90fe64eefacb02f32b7cbc625fc8401d86db0b
Maxim <mailtime2010@gmail.com>
2019-01-08 11:06:15 -0500

    1. Singleton.[#99454] package-info.java

---
+++ chapter_002/src/main/java/ru/job4j/singletons/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * TrackerSingletonEnum, TrackerSingletonEager, TrackerSingletonLazy, TrackerSingletonLazyInner.
+ *
+ * @author Maxim Vanny.
+ * @version 2.0
+ * @since 0.1
+ */
+package ru.job4j.singletons;
