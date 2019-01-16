2975188c39ed28a3ace3b4540ff805e1413f8421
Maxim <mailtime2010@gmail.com>
2019-01-16 14:53:57 -0500

    4.5.1 Peer review. Template

---
+++ chapter_003/src/main/java/account/Sorter.java
@@ -0,0 +1,45 @@
+package account;
+
+
+import java.util.*;
+
+public class Sorter {
+
+    public Sorter(){
+
+    }
+
+    Set<User> sort (List<User> list) {
+        TreeSet<User> sortedList = new TreeSet<>();
+        sortedList.addAll(list);
+        return sortedList;
+    }
+
+    List<User> sortnamelength (List<User> list) {
+        Comparator<User> compar = new Comparator<User>() {
+            @Override
+            public int compare (User o1, User o2) {
+                return o1.getName().length() - o2.getName().length();
+            }
+        };
+        list.sort(compar);
+        return list;
+    }
+
+    List<User> sortboth (List<User> list) {
+        Comparator<User> compar1 = new Comparator<User>() {
+            @Override
+            public int compare (User o1, User o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        };
+        Comparator<User> compar2 = new Comparator<User>() {
+            @Override
+            public int compare (User o1, User o2) {
+                return o1.getAge() - o2.getAge();
+            }
+        };
+        list.sort(compar1.thenComparing(compar2));
+        return list;
+    }
+}
