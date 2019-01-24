a9457ceaf0cba87c0d90e19a8c4a4a6afa87f72d
Maxim <mailtime2010@gmail.com>
2019-01-24 12:44:02 -0500

    4.2. В классе StartUI заменить вывод в консоль на Consumer. exception

---
+++ chapter_004/src/main/java/storage/exception/MenuOutException.java
@@ -0,0 +1,19 @@
+package storage.exception;
+
+/**
+ * MenuOutException.
+ *
+ * @author Maxim Vanny.
+ * @version 2.0
+ * @since 0.1
+ */
+public class MenuOutException extends RuntimeException {
+    /**
+     * Constructor.
+     *
+     * @param message message for users.
+     */
+    public MenuOutException(String message) {
+        super(message);
+    }
+}
