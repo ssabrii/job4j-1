e94c46d89d8d62a214aa6b5cca81c7537c1cd3ed
Maxim <mailtime2010@gmail.com>
2019-01-31 18:52:34 -0500

    4.2.1 Stream. package-info.java

---
+++ chapter_004/src/main/java/company/package-info.java
@@ -0,0 +1,11 @@
+/**
+
+ * Profiles.
+ * Profile.
+ * Address.
+ *
+ * @author Maxim Vanny.
+ * @version 4.0
+ * @since 0.1
+ */
+package company;
---
+++ chapter_004/src/main/java/school/package-info.java
@@ -0,0 +1,10 @@
+/**
+
+ * School.
+ * Student.
+ *
+ * @author Maxim Vanny.
+ * @version 4.0
+ * @since 0.1
+ */
+package school;
