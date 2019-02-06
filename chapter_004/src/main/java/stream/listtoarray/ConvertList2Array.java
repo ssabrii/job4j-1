6a6b418cc8eb1de68eb855945c3e46831e1b173b
Maxim <mailtime2010@gmail.com>
2019-02-06 17:05:04 -0500

    4.2.3.1 clean

--- chapter_004/src/main/java/stream/listtoarray/ConvertList2Array.java
+++ chapter_004/src/main/java/stream/listtoarray/ConvertList2Array.java
@@ -23,15 +23,11 @@ public class ConvertList2Array {
      */
     public final Integer[][] toArray(final List<Integer> list, final int rows) {
 
-        int cells;
-        if (list.size() % rows == 0) {
-            cells = list.size() / rows;
-        } else {
-            cells = (list.size() / rows) + 1;
-        }
+        final int cells = list.size() % rows == 0 ?
+                list.size() / rows :
+                list.size() / rows + 1;
+
         final int[] count = {0};
-
-
         return IntStream.range(0, rows)
                 .mapToObj(x -> IntStream.range(0, cells)
                         .mapToObj(y -> count[0] >= list.size()
