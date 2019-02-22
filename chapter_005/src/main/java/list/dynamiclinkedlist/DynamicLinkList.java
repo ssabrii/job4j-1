7b404f806377ef694a39a80b1837a3e639d0eb32
Maxim <mailtime2010@gmail.com>
2019-02-22 17:50:28 -0500

    5.3.2. Создать контейнер на базе связанного списка [#112928]

---
+++ chapter_005/src/main/java/list/dynamiclinkedlist/DynamicLinkList.java
@@ -0,0 +1,109 @@
+package list.dynamiclinkedlist;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+/**
+ * DynamicLinkList.
+ *
+ * @param <T> param T.
+ * @author Maxim Vanny
+ * @version 5.0
+ * @since 2/22/2019
+ */
+public class DynamicLinkList<T> implements Iterable<T> {
+    /**
+     * countMod.
+     */
+    private int countMod;
+    /**
+     * Size.
+     */
+    private int size;
+    /**
+     * First Node.
+     */
+    private Node<T> first;
+
+    /**
+     * Method add.
+     *
+     * @param value value
+     */
+    public final void add(final T value) {
+        Node<T> newNode = new Node<>(value);
+        newNode.next = this.first;
+        this.first = newNode;
+        this.size++;
+        this.countMod++;
+    }
+
+    /**
+     * Method Get.
+     *
+     * @param index index
+     * @return data
+     */
+    public final T get(final int index) {
+        if (index > this.size) {
+            throw new UnsupportedOperationException("Element missing");
+        }
+        Node<T> result = this.first;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result.data;
+    }
+
+    @Override
+    public final Iterator<T> iterator() {
+        return new Iterator<>() {
+            private final int modificationCountMod = countMod;
+            private final Node<T> result = first;
+            private int count = 0;
+            private T data;
+
+            @Override
+            public final boolean hasNext() {
+                if (countMod != this.modificationCountMod) {
+                    throw new ConcurrentModificationException();
+                }
+                return first.next != null;
+            }
+
+            @Override
+            public final T next() {
+                if (!this.hasNext()) {
+                    throw new UnsupportedOperationException();
+                }
+
+                return result.next.data;
+            }
+        };
+    }
+
+    /**
+     * class Node.
+     *
+     * @param <T> any Т
+     */
+    private static class Node<T> {
+        /**
+         * field Node.
+         */
+        private Node<T> next;
+        /**
+         * field data.
+         */
+        private final T data;
+
+        /**
+         * Constructor.
+         *
+         * @param aData data
+         */
+        Node(final T aData) {
+            this.data = aData;
+        }
+    }
+}
---
+++ chapter_005/src/test/java/list/dynamiclinkedlist/DynamicLinkListTest.java
@@ -0,0 +1,59 @@
+package list.dynamiclinkedlist;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * .
+ *
+ * @author Maxim Vanny
+ * @version 5.0
+ * @since 2/22/2019
+ */
+public class DynamicLinkListTest {
+    DynamicLinkList<Integer> dynamic = new DynamicLinkList<>();
+
+    @Before
+    public void setUpBefore() {
+        this.dynamic.add(1);
+        this.dynamic.add(2);
+        this.dynamic.add(3);
+    }
+
+    @Test
+    public void add() {
+        this.dynamic.add(4);
+        var result = this.dynamic.get(0);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenGetOk() {
+        var result = this.dynamic.get(0);
+        assertThat(result, is(3));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void whenGetOutIndexSize() {
+        var result = this.dynamic.get(10);
+    }
+
+    @Test
+    public void iterator() {
+        Iterator<Integer> iterator = this.dynamic.iterator();
+        assertTrue(iterator.hasNext());
+        assertThat(iterator.next(), is(3));
+        assertTrue(iterator.hasNext());
+        assertThat(iterator.next(), is(2));
+        assertTrue(iterator.hasNext());
+        assertThat(iterator.next(), is(1));
+        assertFalse(iterator.hasNext());
+    }
+}
