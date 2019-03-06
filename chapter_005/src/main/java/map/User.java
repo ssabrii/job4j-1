baf16f8718e858d5a2da7a46f6181a4bd1bb3556
Maxim <mailtime2010@gmail.com>
2019-03-06 05:23:36 -0500

    1. Создать модель User[#112945]

---
+++ chapter_005/src/main/java/map/User.java
@@ -0,0 +1,39 @@
+package map;
+
+import java.util.Calendar;
+
+/**
+ * User.
+ *
+ * @author Maxim Vanny
+ * @version 5.0
+ * @since 3/6/2019
+ */
+public class User {
+    /**
+     * field name.
+     */
+    private final String name;
+    /**
+     * field children.
+     */
+    private final int children;
+    /**
+     * field birthday.
+     */
+    private final Calendar birthday;
+
+    /**
+     * Constructor.
+     *
+     * @param aName     name
+     * @param aChildren children
+     * @param aBirthday birthday
+     */
+    public User(final String aName, final int aChildren,
+                final Calendar aBirthday) {
+        this.name = aName;
+        this.children = aChildren;
+        this.birthday = aBirthday;
+    }
+}
