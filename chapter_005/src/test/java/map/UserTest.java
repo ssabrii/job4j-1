89463ac9584b981cc00dee615c18c36de3251adb
Maxim <mailtime2010@gmail.com>
2019-03-06 05:30:07 -0500

    2. Не перекрывать equals hashCode[#112949]

---
+++ chapter_005/src/test/java/map/UserTest.java
@@ -0,0 +1,24 @@
+package map;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class UserTest {
+
+    @Test
+    public void whenPutToMap() {
+        final Calendar birthday = Calendar.getInstance();
+        birthday.set(1945, Calendar.MAY, 9);
+        final User one = new User("Suzy", 2, birthday);
+        final User two = new User("Suzy", 2, birthday);
+        final Map<User, Object> catalog = new HashMap<>();
+        catalog.put(one, "first");
+        catalog.put(two, "second");
+        System.out.println(catalog);
+    }
+
+}
